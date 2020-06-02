0b7530ffd9920b85d39ad154d1c700d8adddbe25
Guy Shoham <guyshoham28@gmail.com>
2020-06-02 14:56:09 +0300

    create threadPool.c

---
+++ threadPool.c
@@ -0,0 +1,17 @@
+//
+// Created by guy on 02/06/2020.
+//
+
+#include <stddef.h>
+#include "threadPool.h"
+
+ThreadPool* tpCreate(int numOfThreads) {
+  return NULL;
+}
+void tpDestroy(ThreadPool* threadPool, int shouldWaitForTasks) {
+
+}
+int tpInsertTask(ThreadPool* threadPool, void (* computeFunc)(void*), void* param) {
+  return 0;
+}
+
